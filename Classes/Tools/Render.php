612035c7475a9399473602edf0c0affebb6a96b1
Christian Händel <christian@vinou.de>
2019-09-06 11:56:03 +0200

    [ADD] filesize filter

--- Classes/Tools/Render.php
+++ Classes/Tools/Render.php
@@ -205,6 +205,44 @@ class Render {
             return gettype($var);
         }));
 
+        $twig->addFilter( new \Twig_SimpleFilter('filesize', function ($file) {
+            $file = Helper::getNormDocRoot().$file;
+            $bytes = floatval(filesize($file));
+            $arBytes = array(
+                0 => array(
+                    "UNIT" => "TB",
+                    "VALUE" => pow(1024, 4)
+                ),
+                1 => array(
+                    "UNIT" => "GB",
+                    "VALUE" => pow(1024, 3)
+                ),
+                2 => array(
+                    "UNIT" => "MB",
+                    "VALUE" => pow(1024, 2)
+                ),
+                3 => array(
+                    "UNIT" => "KB",
+                    "VALUE" => 1024
+                ),
+                4 => array(
+                    "UNIT" => "B",
+                    "VALUE" => 1
+                ),
+            );
+
+            foreach($arBytes as $arItem)
+            {
+                if($bytes >= $arItem["VALUE"])
+                {
+                    $result = $bytes / $arItem["VALUE"];
+                    $result = str_replace(".", "," , strval(round($result, 2)))." ".$arItem["UNIT"];
+                    break;
+                }
+            }
+            return $result;
+        }));
+
         //filter for decimal->brutto conversion 123.456 => 123.46
         $twig->addFilter( new \Twig_SimpleFilter('brutto', function($decimal) {
             return $decimal;
