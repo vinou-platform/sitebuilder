2a7a9c9a962722965a76d450a6441c1436c71bd2
Christian Händel <christian@vinou.de>
2020-02-22 23:19:20 +0100

    [ADD] bytes viewhelper

--- Classes/Tools/Render.php
+++ Classes/Tools/Render.php
@@ -365,6 +365,20 @@ class Render {
             return $result;
         }));
 
+        $twig->addFilter( new \Twig_SimpleFilter('bytes', function ($bytes, $precision = 2) { 
+            $units = array('B', 'KB', 'MB', 'GB', 'TB'); 
+
+            $bytes = max($bytes, 0); 
+            $pow = floor(($bytes ? log($bytes) : 0) / log(1024)); 
+            $pow = min($pow, count($units) - 1); 
+
+            // Uncomment one of the following alternatives
+            $bytes /= pow(1024, $pow);
+            // $bytes /= (1 << (10 * $pow)); 
+
+            return round($bytes, $precision) . ' ' . $units[$pow]; 
+        }));
+
         //filter for decimal->brutto conversion 123.456 => 123.46
         $twig->addFilter( new \Twig_SimpleFilter('brutto', function($decimal) {
             return $decimal;
